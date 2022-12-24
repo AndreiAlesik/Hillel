11c500c280d4eb2071c22dc4314d29f761a7445d
Andrew <olesik.andrei@gmail.com>
2022-12-24 15:04:49 +0100

    abc

---
+++ scr_lecture_code/application/src/Cafe.java
@@ -0,0 +1,14 @@
+public class Cafe {
+    private String name;
+    private String address;
+    private int drinks;
+
+    public Cafe() {
+    }
+
+    public Cafe(String name, String address, int drinks) {
+        this.name = name;
+        this.address = address;
+        this.drinks = drinks;
+    }
+}
