3e12bf58b4f8a29c9a141f236051d6df7eebf4a3
Andrew <olesik.andrei@gmail.com>
2023-01-13 10:45:35 +0100

    Java Collections + Homework

--- scr_lecture_code/application/src/Cafe.java
+++ scr_lecture_code/application/src/Cafe.java
@@ -1,17 +1,32 @@
+import java.util.Objects;
+
 public class Cafe {
     private String name;
     private String address;
     private int drinks;
 
+
     public Cafe() {
     }
+
+
 
     public Cafe(String name, String address, int drinks) {
         this.name = name;
         this.address = address;
         this.drinks = drinks;
     }
+    public String getName() {
+        return name;
+    }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public int getDrinks() {
+        return drinks;
+    }
     @Override
     public String toString() {
         return "Cafe{" +
@@ -19,5 +34,18 @@ public class Cafe {
                 ", address='" + address + '\'' +
                 ", drinks=" + drinks +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cafe)) return false;
+        Cafe cafe = (Cafe) o;
+        return drinks == cafe.drinks && name.equals(cafe.name) && address.equals(cafe.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address, drinks);
     }
 }
--- scr_lecture_code/application/src/Main.java
+++ scr_lecture_code/application/src/Main.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Cafe cafe1=new Cafe("1","11",12);
         Cafe cafe2=new Cafe("2","22",22);
+        Cafe cafe3=new Cafe("2","22",22);
 
         List<Cafe> cafeList=new ArrayList<>();
 
         cafeList.add(cafe1);
         cafeList.add(cafe2);
+        cafeList.add(cafe3);
         System.out.println(findFirst(cafeList));
         System.out.println();
+        System.out.println();
 
+        System.out.println("-----------Homework------------");
+        System.out.println("Element is exist: "+isExist(cafeList, "3"));
+        System.out.println("Element is exist: "+isExist(cafeList, "2"));
+        System.out.println("Items number: "+listLength(cafeList));
+        System.out.println("Is duplicated "+isDuplicatedItemsExist(cafeList));
     }
     public static Cafe findFirst(List<Cafe> cafeList) throws Exception {
         try{
@@ -38,5 +47,34 @@ public class Main {
         Iterator<Cafe> itr=cafeList.iterator();
         boolean check=itr.hasNext();
         return check;
+    }
+    public static boolean isExist(List<Cafe> cafeList, String cafeName){
+        boolean answer=false;
+        for(Cafe items: cafeList){
+            if(items.getName().equals(cafeName))
+                answer=true;
+        }
+        return answer;
+    }
+
+    public static boolean isDuplicatedItemsExist(List<Cafe> cafeList){
+        List<Cafe> uniqueItems = new ArrayList<>();
+        int count=0;
+        for (Cafe items : cafeList)
+            //finding unique Structures and add to unique list
+            if (cafeList.stream().filter(items::equals).count() == 1) {
+                uniqueItems.add(items);
+            }
+        List<Cafe> uniq = cafeList.stream().distinct().collect(Collectors.toList());
+        for (Cafe items : uniq) {
+            //set Occurrence in Structures and print them
+            count=((int) (cafeList.stream().filter(items::equals).count()));
+
+
+        }
+        if(count==1)
+            return false;
+        else
+            return true;
     }
 }
